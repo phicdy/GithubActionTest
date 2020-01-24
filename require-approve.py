55bcfafc06fd3e48a3e7f01ac87e6ba7f2e238a4
phicdy <phicdy@gmail.com>
2020-01-24 21:01:21 +0900

    refactor

--- require-approve.py
+++ require-approve.py
@@ -1,49 +1,70 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import base64
 import json
 import os
-import random
+import sys
 import urllib.request
 import urllib.parse
 
 GITHUB_TOKEN = os.environ.get('GITHUB_TOKEN')
+
 
 def create_github_request(url, data=None):
     req = urllib.request.Request(url, data)
     req.add_header('Authorization', f'token {GITHUB_TOKEN}')
     return req
 
-# open event.json
+
+def post_github_request(url, data):
+    req_statuses = create_github_request(url, json.dumps(data).encode())
+    with urllib.request.urlopen(req_statuses) as response:
+        print(response)
+
+
 with open(os.environ.get('GITHUB_EVENT_PATH')) as f:
     event = json.load(f)
     pr_event = event["pull_request"]
-    print(pr_event)
 
-# collaborators_endpoint = event["repository"]["collaborators_url"].split("{")[0]
+    reviews_endpoint = pr_event["_links"]["self"]["href"] + "/reviews"
+    statuses_endopint = pr_event["_links"]["statuses"]["href"]
 
-# req = create_github_request(collaborators_endpoint)
-# with urllib.request.urlopen(req) as response:
-#     res = json.load(response)
-#     new_reviewer = random.choice(res)
+    req = create_github_request(reviews_endpoint)
+    with urllib.request.urlopen(req) as response:
+        res = json.load(response)
+        if res == []:
+            data = {
+                "state": "failure",
+                "description": "No review",
+                "context": "approve check"
+            }
+            post_github_request(statuses_endopint, data)
+            exit()
 
-#     reviewers.append(new_reviewer)
+        for review in res:
+            user = review["user"]["login"]
+            print(user)
+            if user != "kanakohonda550":
+                continue
+            if review["state"] == "APPROVED":
+                data = {
+                    "state": "success",
+                    "description": "QA approvement",
+                    "context": "approve check"
+                }
+                post_github_request(statuses_endopint, data)
+            else:
+                data = {
+                    "state": "failure",
+                    "description": "Wait for QA approvement",
+                    "context": "approve check"
+                }
+                post_github_request(statuses_endopint, data)
+            exit()
 
-#     reviewers_name = map(lambda x: x["login"], reviewers)
-
-#     # POST /repos/:owner/:repo/pulls/:number/requested_reviewers
-#     url = f'{pr_event["url"]}/requested_reviewers'
-#     body = { "reviewers": list(reviewers_name), "team_reviewers": [] }
-#     data = json.dumps(body).encode()
-#     print(body)
-
-#     req = create_github_request(url, data)
-#     req.add_header('Content-Type', 'application/json')
-
-#     try:
-#         urllib.request.urlopen(req)
-#     except urllib.error.HTTPError as err:
-#         print(err.read())
-
-exit()
+        data = {
+            "state": "failure",
+            "description": "QA is not assigned",
+            "context": "approve check"
+        }
+        post_github_request(statuses_endopint, data)
